75b621ed2aa3a6cd307449f2f5a8c061c7d29821
Ac1d <974341189@qq.com>
2021-07-06 14:23:47 +0800

    前端基本完成

--- canaryController/canaryController/settings.py
+++ canaryController/canaryController/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'canaryController.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HoneyPot',
+        'USER': 'root',
+        'PASSWORD': 'zx123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
